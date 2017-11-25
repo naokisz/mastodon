fd13c5da1df3fcc02da86bfc4fc05b832e12821b
霧島ひなた <8372135+Kirishima21@users.noreply.github.com>
2017-11-26 03:28:38 +0900

    Revert "Revert "Revert "DirectTimeLineの追加"""
    This reverts commit 23e1c0526e2e861a90284aa5b70b52da343f1ead.

--- app/javascript/mastodon/actions/streaming.js
+++ app/javascript/mastodon/actions/streaming.js
@@ -92,4 +92,3 @@ export const connectCommunityStream = () => connectTimelineStream('community', '
 export const connectMediaStream = () => connectTimelineStream('community', 'public:local');
 export const connectPublicStream = () => connectTimelineStream('public', 'public');
 export const connectHashtagStream = (tag) => connectTimelineStream(`hashtag:${tag}`, `hashtag&tag=${tag}`);
-export const connectDirectStream = () => connectTimelineStream('direct', 'direct'); 
